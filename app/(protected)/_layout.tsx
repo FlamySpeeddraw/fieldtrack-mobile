b0faadbe5e38a1d856a56694b2fac9057bcda5c7
Sacha <sachaobiedzy@gmail.com>
2025-12-03 08:49:51 +0100

    Refactor: headerShown false dans le stack

--- app/(protected)/_layout.tsx
+++ app/(protected)/_layout.tsx
@@ -2,8 +2,8 @@ import { Stack } from 'expo-router';
 
 const ProtectedLayout = () => {
     return (
-        <Stack>
-            <Stack.Screen name='interventionList' options={{ headerShown: false }} />
+        <Stack screenOptions={{ headerShown: false }}>
+            <Stack.Screen name='interventionList' />
         </Stack>
   );
 };
